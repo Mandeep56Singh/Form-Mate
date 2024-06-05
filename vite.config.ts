a9906cdd0cff291eb8b39ec8d2ba246aae59b40c
mandeepSingh <mandeepraj2312@gmail.com>
2024-06-05 22:13:07 +0530

    build(UI) : setup shadcn ui for vite ts environment
    Installed the Shad CN library to enhance our UI components. This commit includes the following changes:
    
    - Added Shad CN as a dependency in the package.json file.
    - Updated build scripts and configuration to integrate Shad CN with our project.
    - configure vite for Shad CN
    - configure tailwind for Shad CN
    -Update tsconfig.json for shadcn ui
    - created components.json for future components installation
    - Added CSS variables in index.css
    - add lib directory containg twMerge functionality
    - Ensured compatibility with existing build processes and tested integration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from './manifest.json'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import manifest from "./manifest.json";
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
- 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
